d79cb17665dbf79d91ef4546504dcf61e5c11f60
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-09-25 18:12:43 +0545

    feat: add products to a store

--- medusa-store/src/models/product.ts
+++ medusa-store/src/models/product.ts
@@ -1,8 +1,14 @@
-import { Column, Entity } from "typeorm";
-
+import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
 import { Product as MedusaProduct } from "@medusajs/medusa";
+import { Store } from "./store";
 
 @Entity()
 export class Product extends MedusaProduct {
-  // add relation to a store
+  @Index("ProductStoreId")
+  @Column({ nullable: true })
+  store_id?: string;
+
+  @ManyToOne(() => Store, (store) => store.products)
+  @JoinColumn({ name: "store_id", referencedColumnName: "id" })
+  store?: Store;
 }
